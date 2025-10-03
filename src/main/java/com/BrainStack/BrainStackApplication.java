611b9a350761ceb50c37e8a8d75ceb934c9b7f95
kalaiamine1 <medamine.kalai@esprit.com>
2025-10-03 15:33:46 +0100

    Initial commit - NeuroCare Backend

---
+++ src/main/java/com/BrainStack/BrainStackApplication.java
@@ -0,0 +1,13 @@
+package com.BrainStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BrainStack/BrainStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BrainStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
